Fix data normalization

helpers.py
@@ -2,7 +2,11 @@ import numpy as np
 
 
 def normalize_data(data):
-    max_element = np.max(data)
-    min_element = np.min(data)
-    normalized_data = np.divide(data - min_element, max_element - min_element)
-    return normalized_data
+    normalized_data = np.empty(data.T.shape)
+    for idx, col in enumerate(data.T):
+        max_element = np.max(col)
+        min_element = np.min(col)
+        normalized_data[idx, :] = np.divide(
+            col - min_element, max_element - min_element
+        )
+    return normalized_data.T